3b35380e4edb072957d03b3ab32d2673d3c24e9a
Will Button <will@willbutton.co>
2018-04-12 19:41:35 -0700

    Cleanup for boxing

--- migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-var Rental = artifacts.require("./Rental.sol");
-
-module.exports = function(deployer) {
-    deployer.deploy(Rental);
-};
--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import React, { Component } from 'react'
-import Rental from '../build/contracts/Rental.json'
 import getWeb3 from './utils/getWeb3'
 
-import './css/oswald.css'
-import './css/open-sans.css'
 import './css/pure-min.css'
 import './css/grids-responsive-min.css'
 import './App.css'
 
 import rentals from './data/rentals.json'
-
-// function RentalItem(props) {
-//     return (
-//       <div key={props.rental.id} className="pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
-//           <h3>{props.rental.model}</h3>
-//           <h4>{props.rental.year} {props.rental.make}</h4>
-//           <img src={require(props.rental.image)} alt="Car props.rental" width="100" />
-//           <button 
-//             onClick={() => this.handleRentMe(props.rental.id)}
-//             className="pure-button pure-button-primary">
-//             Rent Me
-//           </button>
-//       </div>
-//     )
-// }
 
 class RentalItem extends Component {
     constructor(props) {
@@ -32,9 +14,6 @@ class RentalItem extends Component {
     }
 
     handleClick(id) {
-        // this.setState({
-        //     disabled: !this.state.disabled
-        // })
         this.props.handleRentMe(id)
     }
 
@@ -48,7 +27,7 @@ class RentalItem extends Component {
                     onClick={() => this.handleClick(this.props.id)} 
                     className="pure-button pure-button-primary"
                     disabled={this.props.disabled}>
-                    Rent Me
+                    Rent Me for Ξ{this.props.price}
                 </button>
             </div>
         )
@@ -86,104 +65,36 @@ class App extends Component {
   }
 
   instantiateContract() {
-    const contract = require('truffle-contract')
-    const rental = contract(Rental)
-    rental.setProvider(this.state.web3.currentProvider)
-    let rentalInstance
-    this.state.web3.eth.getAccounts((error, accounts) => {
-        rental.deployed().then((instance) => {
-            rentalInstance = instance
-            return rentalInstance.getRentals.call()
-        }).then((result) => {
-            console.log('Rental Status on init')
-            console.log(result)
-            this.setState({
-                renters: result
-            })
-            for (let i = 0; i < result.length; i++) {
-                if (result[i] !== '0x0000000000000000000000000000000000000000') {
-                    this.markRented(i)
-                }
-            }
-        }).catch((err) => {
-            console.log('Error listing rentals')
-            console.log(err)
-        })
-    })
+    // add your code to handle initial page load here
+    console.log('instantiateContract fired')
   }
 
   handleRentMe(id) {
-      console.log(`Clicked button ${id}`)
-      let rentalInstance
-      const contract = require('truffle-contract')
-      const rental = contract(Rental)
-      rental.setProvider(this.state.web3.currentProvider)
-      this.state.web3.eth.getAccounts((error, accounts) => {
-          rental.deployed().then((instance) => {
-              console.log(`accounts: ${accounts[0]}`)
-              rentalInstance = instance
-              return rentalInstance.rent(id, {from: accounts[0]})
-          }).then((result) => {
-              console.log('Successfully rented')
-              console.log(result)
-              this.handleGetRentals()
-          }).catch((err) => {
-              console.log('Error renting car')
-              console.log(err)
-          })
-      })
+      // add code to rent vehicle here
+      console.log('handleRentMe fired')
   }
 
   handleGetRentals() {
-      console.log('Refreshing rental state from network')
-      let rentalInstance
-      const contract = require('truffle-contract')
-      const rental = contract(Rental)
-      rental.setProvider(this.state.web3.currentProvider)
-      this.state.web3.eth.getAccounts((err, accounts) => {
-          rental.deployed().then((instance) => {
-              rentalInstance = instance
-              return rentalInstance.getRentals.call()
-          }).then((result) => {
-              console.log(result)
-          }).catch((err) => {
-              console.log('Error fetching rentals')
-              console.log(err)
-          })
-      })
+      // add code to get all rentals here
+      console.log('handleGetRentals fired')
   }
 
   handleGetRentalById(id) {
-      console.log('Getting this rental')
-      let rentalInstance
-      const contract = require('truffle-contract')
-      const rental = contract(Rental)
-      rental.setProvider(this.state.web3.currentProvider)
-      this.state.web3.eth.getAccounts((err, accounts) => {
-          rental.deployed().then((instance) => {
-              rentalInstance = instance
-              return rentalInstance.rentals(id)
-          }).then((result) => {
-              console.log(result)
-          }).catch((err) => {
-              console.log('Error getting rental')
-              console.log(err)
-          })
-      })
-  }
-
-  markRented(id) {
-      console.log(`Rental ${id} should be marked as rented`)
+      // add code to get a single rental here
+      console.log('handleGetRentalById fired')
   }
 
   isRented(id, available) {
-      console.log(id)
-      console.log(this.state.renters[id])
       if (available === true && this.state.renters[id] === '0x0000000000000000000000000000000000000000') {
           return false
       } else {
           return true
       }
+  }
+
+  getContractBalance() {
+      // add code to get Ether value of contract here
+      console.log('getContractBalance fired')
   }
 
   render() {
@@ -197,7 +108,7 @@ class App extends Component {
         <main className="container">
           <div className="pure-g">
             <div className="pure-u-1 pure-u-lg-1-2"><h1>Rentals</h1></div>
-            <div className="pure-u-1 pure-u-lg-1-2"><p>View all rentals here. If available, you may rent a car by selecting the "Rent Now" button. Your account will be debited 5eth.</p></div>
+            <div className="pure-u-1 pure-u-lg-1-2"><p>View all rentals here. If available, you may rent a car by selecting the "Rent Now" button.</p></div>
             {rentals.map( (rental) => {
                 return (
                     <RentalItem 
@@ -207,10 +118,10 @@ class App extends Component {
                         model={rental.model}
                         year={rental.year}
                         image={rental.image}
+                        price={rental.price}
                         disabled={this.isRented(rental.id, rental.available)}
                         handleRentMe={this.handleRentMe}/>
                     )
-                // return <RentalItem key={rental.id} rental={rental} onClick={this.handleRentMe} />
             })}
           </div>
         </main>
